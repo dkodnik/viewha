8cd3d4624bf267524df48b39620ed5ee66f0c5f6
dkodnik <dkodnik@gmail.com>
2013-07-09 17:46:26 +0400

    add favicon and helping

--- vm.js
+++ vm.js
@@ -9,12 +9,15 @@ jQuery(function($){
 		perPage		: 8,			// A maximum of 8 is allowed by Google
 		page		: 0				// The start page
 	};
+	var viewTypeData=false; // тип строки поиска для показа с результатом поиска?
+	var timerHelpUsr=0;
+	var helpViewUsr=false; // подсказка показывается пользователю?
+	var srcLineFocus=true; // строка ввода запроса в фокусе? (по умолчанию на #s установлен "autofocus")
+
 	var cursor=0;
 	var sttngs={};
-
-	setTimeout(function(){
-		$('#logosrch small').show("slow");
-	}, 3000); // 3sec
+	
+	
 	
 	function googleSearch(settings){
 		
@@ -61,16 +64,6 @@ jQuery(function($){
 				pageContainer.appendTo(resultsDiv);
 				
 				cursor = r.responseData.cursor;
-				 
-				
-				/*$('#page').scroll(function(){
-					if ($('#page').scrollTop() == $('#resultsDiv').height() - $('#page').height()){
-						if( +cursor.estimatedResultCount > (settings.page+1)*settings.perPage){
-							googleSearch({append:true,page:settings.page+1});
-						}
-					}
-				});*/
-				
 
 				$('.webResult').on('click',function(){
 					var el = $(this);
@@ -115,10 +108,82 @@ jQuery(function($){
 			$('#searchForm').removeClass('cn fix').addClass('fix');
 			$('#clearButton').css('display','block');
 			$("#logosrch").css('display','none');
+			viewTypeData=true;
+			hideHelp();
 
 			googleSearch();
 		}
 	}
+
+	function hideHelp() {
+		helpViewUsr=false;
+		timerHelpUsr=0;
+		$('.help_left p').hide();
+		$('.help_left small').hide();
+		$('.help_right p').hide();
+		$('.help_right small').hide();
+	}
+
+	setInterval(function(){
+		if(!viewTypeData & !helpViewUsr) {
+			timerHelpUsr++;
+
+			// через 20сек. показываем пользователю подсказки
+			if(timerHelpUsr>20) {
+				helpViewUsr=true;
+				timerHelpUsr=0;
+				if($('#s').val()!='') {
+					// Что-то ввели
+					if(srcLineFocus) {
+						// предлогаем нажать клавишу enter или по синей кнопке со стрелочкой ">"
+						$('.help_right small').html('нажмите клавишу "Enter &#x23ce;" или синию кнопку со стрелкой ">", для поиска по запросу');
+						$('.help_right p').show("slow");
+						$('.help_right small').show("slow");
+					} else {
+						// предлогаем нажать по синей кнопке со стрелочкой ">"
+						$('.help_right small').html('нажмите синию кнопку со стрелкой ">", для поиска по запросу');
+						$('.help_right p').show("slow");
+						$('.help_right small').show("slow");
+					}
+				} else {
+					// Не ввели ни чего
+					if(srcLineFocus) {
+						// предлогаем набрать текст запроса
+						$('.help_left small').html('(1) - сформулируйте свой запрос и введите сюда');
+						$('.help_right small').html('(2) - нажмите синию кнопку со стрелкой ">" для поиска по запросу или клавишу "Enter &#x23ce;"');
+						$('.help_left p').show("slow");
+						$('.help_left small').show("slow",function(){
+							$('.help_right p').show("slow");
+							$('.help_right small').show("slow");
+						});
+					} else {
+						// предлогаем нажать на строку поиска и набрать текст
+						$('.help_left small').html('нажмите на строку поиска и введите сюда свой запрос');
+						$('.help_left p').show("slow");
+						$('.help_left small').show("slow");
+					}
+				}
+			}
+
+		}
+	}, 1000); // 1sec
+
+	setTimeout(function(){
+		$('#logosrch small').show("slow");
+	}, 5000); // 5sec
+
+	$('#s').bind('keyup',function(){
+		hideHelp();
+	});
+
+	$('#s').focus(function(){
+		srcLineFocus=true;
+		hideHelp();
+	});
+	$('#s').blur(function(){
+		srcLineFocus=false;
+		hideHelp();
+	});
 
 	$('#page').scroll(function(){
 		if ($('#page').scrollTop() == $('#resultsDiv').height() - $('#page').height()){
@@ -139,6 +204,7 @@ jQuery(function($){
 		$("#s").val('');
 		$("#logosrch").css('display','block');
 		$('#searchForm').removeClass('cn fix').addClass('cn');
+		viewTypeData=false;
 	});
 
 	// parse url path..
